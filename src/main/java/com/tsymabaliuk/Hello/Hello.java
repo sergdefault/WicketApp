39098aabb79612aa343694f402d70124938597c6
sergdefault <sergdefault@gmail.com>
2017-01-16 18:41:26 +0200

    svn commit

--- src/main/java/com/tsymabaliuk/Hello/Hello.java
+++ src/main/java/com/tsymabaliuk/Hello/Hello.java
@@ -24,7 +24,7 @@ public class Hello extends WebPage {
                 return counter;
             }
         };
-        // test message
+
         Label label = new Label("counter", counterModel);
         add(label);
 
